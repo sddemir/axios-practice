c0eaf375a1e6e0d9db22da29343fb36558e9d5ed
Sümeyye Demir <sumeyyedemir1903@gmail.com>
2023-08-17 14:00:27 +0300

    it works!

--- ah.js
+++ ah.js
@@ -1,1 +1,7 @@
-console.log("of");
+const form= document.querySelector('form');
+// const input=document.querySelector('input');
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const showTitle=form.elements.query.value
+    console.log(showTitle);
+})
