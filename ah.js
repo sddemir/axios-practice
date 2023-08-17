1abd6603050f7e0811d3db2a2abd478a02541186
Sümeyye Demir <sumeyyedemir1903@gmail.com>
2023-08-17 15:07:00 +0300

    search show names

--- ah.js
+++ ah.js
@@ -1,7 +1,16 @@
 const form= document.querySelector('form');
 // const input=document.querySelector('input');
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', async function(e){
     e.preventDefault();
-    const showTitle=form.elements.query.value
-    console.log(showTitle);
+    const showTitle=form.elements.query.value;
+    // showTitle='';
+    const res =await axios.get(`https://api.tvmaze.com/search/shows?q=${showTitle}`);
+    moreResults(res.data);
 })
+const moreResults= (shows)=>{
+    for(let show of shows){
+        const showName= document.createElement('H1');
+        showName.textContent=show.show.name;
+        document.body.append(showName);
+    }
+}
